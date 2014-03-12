2a7650024d5040c921b8d6ef57bb8c85e973510e
Greg Swift <gregswift@gmail.com>
2014-03-11 21:34:57 -0500

    added package_data and zip_safe for the templates file.  Adjusted the indentation

--- setup.py
+++ setup.py
@@ -8,27 +8,25 @@ VERSION = "0.1"
 SHORT_DESC = "%s tool for to build RPMs from Puppet Forge" % NAME
 LONG_DESC = """
 %s is a python application that downloads releases from Puppet Forge
-and rolls them up into RPMs.  Then it will also generate a usable yum
-repository for managing the version of  modules installed on a system
+and rolls them up into RPMs. Then it will also generate a usable yum
+repository for managing the version of modules installed on a system
 in the same way you can manage your regular software.
 """ % NAME
 
 
 if __name__ == "__main__":
- 
-#        manpath    = "share/man/man1/"
-        setup(
-                name = NAME,
-                version = VERSION,
-                author = "Greg Swift",
-                author_email = "gregswift@gmail.com",
-                url = "https://github.com/gregswift/%s" % NAME,
-                license = "GPLv3",
-                scripts = ["scripts/%s" % NAME],
-                package_dir = {NAME: NAME},
-                packages = [NAME],
-                description = SHORT_DESC,
-                long_description = LONG_DESC
-        )
-
-#                data_files = [(manpath,  ["docs/%s.1.gz" % NAME])],
+    setup(
+        name = NAME,
+        version = VERSION,
+        author = "Greg Swift",
+        author_email = "gregswift@gmail.com",
+        url = "https://github.com/gregswift/%s" % NAME,
+        license = "GPLv3",
+        scripts = ["scripts/%s" % NAME],
+        packages = [NAME],
+        package_dir = {NAME: NAME},
+        package_data = {NAME: ['templates/*.j2']},
+        zip_safe = False,
+        description = SHORT_DESC,
+        long_description = LONG_DESC
+    )
